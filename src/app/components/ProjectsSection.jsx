82df08660bab291ed473f84f88671e50392a582b
Chathuri <chathurinirosha112233@gmail.com>
2025-10-26 19:41:45 +0530

    name changes

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -67,7 +67,7 @@ const ProjectsSection = () => {
     {id: 8,
       title: "Attendmi",
       description: "Attendence Management system designed with figma.",
-      image: "/images/projects/Project8.png",
+      image: "/images/projects/Attendmi.png",
       tag: ["All", "Design"],
       gitUrl: "https://www.figma.com/design/Hclo5aEYW9j1DRyJ4No3gu/AttendMi?node-id=0-1&t=IDW0Z6DDlmkg7xa0-1",
       previewUrl: "https://www.figma.com/design/Hclo5aEYW9j1DRyJ4No3gu/AttendMi?node-id=0-1&t=IDW0Z6DDlmkg7xa0-1",
